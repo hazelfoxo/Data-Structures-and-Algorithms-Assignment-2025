868edb3d70ef81719f6963b7387a182f11cb8d0e
Hazie <3nicksu2005@gmail.com>
2025-12-11 21:24:44 +0000

    implement simulation log

--- 202425Project/Simulator.h
+++ 202425Project/Simulator.h
@@ -5,23 +5,33 @@
 
 class Simulator
 {
+	std::vector<std::list<std::string>> simulationLog;
 public:
 	// This makes the game prototype play against itself using the file and first character specified.
 	std::list<std::string> run(std::string file, char start, int seed) {
-		std::list < std::string> results;
-		return results;
+		Prototype game(file);
+		game.seed(seed);
+		std::list<std::string> cityNameLog;
+		while (true) {
+			std::string city = game.getCity(start);
+			if (city.empty()) break;        
+			game.markUsed(city);             
+			cityNameLog.push_back(city); 
+		}
+		return cityNameLog;
 	}
 
 	// runs k simulations of game and return a double coutning the time to run the batch in seconds. 
 	// Results should be stored internally in a vector list of strings
 	double batch(std::string file, int k, int seed) {
-		return 0;
+		Prototype game(file);
+		game.seed(seed);
+		return 0; // Placeholder return statement
 	}
 
 	//This returns a vector with all simulation results executed so far. Each simulation should be reprensented as a list of strings.
 	std::vector<std::list<std::string>> getResults() {
-		std::vector<std::list<std::string>> results;
-		return results;
+		return simulationLog;
 	}
 };
 
