b29c25edd9c943c74d4d12a0b6a4ca190632f75e
Hazie <3nicksu2005@gmail.com>
2025-12-11 21:34:39 +0000

    update

--- 202425Project/Simulator.h
+++ 202425Project/Simulator.h
@@ -11,21 +11,27 @@ public:
 	std::list<std::string> run(std::string file, char start, int seed) {
 		Prototype game(file);
 		game.seed(seed);
-		std::list<std::string> cityNameLog;
+		std::list<std::string> currentSimulation;
 		while (true) {
 			std::string city = game.getCity(start);
 			if (city.empty()) break;        
 			game.markUsed(city);             
-			cityNameLog.push_back(city); 
+			currentSimulation.push_back(city);
+			
 		}
-		return cityNameLog;
+		if (!currentSimulation.empty()) simulationLog.push_back(currentSimulation);
+		return currentSimulation;
 	}
 
-	// runs k simulations of game and return a double coutning the time to run the batch in seconds. 
+	// runs k simulations of game and return a double counting the time to run the batch in seconds. 
 	// Results should be stored internally in a vector list of strings
 	double batch(std::string file, int k, int seed) {
 		Prototype game(file);
 		game.seed(seed);
+		std::list<std::string> currentSimulation;
+
+
+		simulationLog.push_back(currentSimulation);
 		return 0; // Placeholder return statement
 	}
 
